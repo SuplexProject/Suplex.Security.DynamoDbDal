989e6f4a24e9c56363eccd26b2fd6219eea92503
Tze <yeongtze@googlemail.com>
2018-04-17 03:05:03 -0400

    Added tests for user upsert.

--- Suplex.Security.DynamoDbDal.Tests/DynamoDbDalTests.cs
+++ Suplex.Security.DynamoDbDal.Tests/DynamoDbDalTests.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Amazon.DynamoDBv2.Model;
+using NUnit.Framework;
+using Suplex.Security.Principal;
 
 namespace Suplex.Security.DynamoDbDal.Tests
 {
+    [TestFixture]
     public class DynamoDbDalTests
     {
+        [Test]
+        public void UpsertUser_Valid_Succeeds()
+        {
+            User user = new User()
+            {
+                Name = "User-",
+                IsBuiltIn = true,
+                IsEnabled = true,
+                IsLocal = true
+            };
+            user.Name = user.Name + user.UId;
+
+            DynamoDbDal dal = new DynamoDbDal
+            {
+                TableName = "Suplex.User"
+            };
+            User upsertedUser = dal.UpsertUser(user);
+            Assert.AreEqual(upsertedUser.UId, user.UId);
+            Assert.AreEqual(upsertedUser.Name, user.Name);
+        }
+
+        [Test]
+        public void UpsertUser_NonExistent_Table_Throws_Exception()
+        {
+            User user = new User()
+            {
+                Name = "User-",
+                IsBuiltIn = true,
+                IsEnabled = true,
+                IsLocal = true
+            };
+            user.Name = user.Name + user.UId;
+
+            DynamoDbDal dal = new DynamoDbDal
+            {
+                TableName = Guid.NewGuid().ToString()
+            };
+
+            ResourceNotFoundException ex = Assert.Throws<ResourceNotFoundException>(() => dal.UpsertUser(user));
+            StringAssert.Contains( "Requested resource not found: Table", ex.Message );
+        }
     }
 }
--- Suplex.Security.DynamoDbDal/DynamoDbDal.cs
+++ Suplex.Security.DynamoDbDal/DynamoDbDal.cs
@@ -1,12 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Newtonsoft.Json;
+using Suplex.Security.AclModel;
+using Suplex.Security.AclModel.DataAccess;
+using Suplex.Security.Principal;
 
 namespace Suplex.Security.DynamoDbDal
 {
-    public class DynamoDbDal
+    public class DynamoDbDal : IDataAccessLayer
     {
+        public string TableName { get; set; }
+
+        public Principal.User GetUserByUId(Guid userUId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Principal.User> GetUserByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Principal.User UpsertUser(Principal.User user)
+        {
+            if (user == null)
+                return null;
+
+            if (string.IsNullOrWhiteSpace(TableName))
+                throw new Exception("Dynamo table name must be specified.");
+
+            try
+            {
+                AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+                AmazonDynamoDBClient client = new AmazonDynamoDBClient(clientConfig);
+
+                JsonSerializerSettings settings = new JsonSerializerSettings()
+                {
+                    TypeNameHandling = TypeNameHandling.Objects,
+                    MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead
+                };
+
+                string output = JsonConvert.SerializeObject(user, Formatting.Indented, settings);
+                Document doc = Document.FromJson(output);
+
+                Table table = Table.LoadTable(client, TableName);
+                if (table != null)
+                {
+                    table.PutItem(doc);
+                }
+                else
+                {
+                    throw new Exception($"Dynamo table {TableName} cannot be found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+                throw;
+            }
+            return user;
+        }
+
+        public void DeleteUser(Guid userUId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Principal.Group GetGroupByUId(Guid groupUId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Principal.Group> GetGroupByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Principal.Group UpsertGroup(Principal.Group @group)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGroup(Guid groupUId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GroupMembershipItem> GetGroupMembers(Guid groupUId, bool includeDisabledMembership = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GroupMembershipItem> GetGroupMembership(Guid memberUId, bool includeDisabledMembership = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GroupMembershipItem UpsertGroupMembership(GroupMembershipItem groupMembershipItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGroupMembership(GroupMembershipItem groupMembershipItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISecureObject GetSecureObjectByUId(Guid secureObjectUId, bool includeChildren)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISecureObject GetSecureObjectByUniqueName(string uniqueName, bool includeChildren)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISecureObject UpsertSecureObject(ISecureObject secureObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSecureObject(Guid secureObjectUId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
